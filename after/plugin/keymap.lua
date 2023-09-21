efe65b64e7a76d6a8e61f6f1e414f8da2b708ba0
florian <flo.gludi@gmail.com>
2023-09-21 16:13:44 +0200

    Add keymap for live_grep

--- after/plugin/keymap.lua
+++ after/plugin/keymap.lua
@@ -1,1 +1,19 @@
+local builtin = require('telescope.builtin')
+
 vim.g.mapleader = " "
+
+-- Telescope
+vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
+vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
+vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
+vim.keymap.set('n', '<leader>fh', '<cmd>Telescope help_tags<cr>')
+vim.keymap.set('n', '<leader>fr', '<cmd>Telescope file_browser path=%:p:h select_buffe=true<cr>')
+
+vim.keymap.set('n', '<leader>sG',
+	function()
+		builtin.live_grep { additional_args = function(args)
+			return vim.list_extend(args,
+				{ '--hidden', '--no-ignore' })
+		end }
+	end, { desc = 'Find Grep Everything' })
+
